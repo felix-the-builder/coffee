3092ee4f906301d980e66c72e47cce68f12c1a61
mike69er <Waake1940@einrot.com>
2018-04-14 00:49:49 +0200

    Alternative Form in Tabelle funktioniert

--- coffee2.php
+++ coffee2.php
@@ -13,7 +13,7 @@ table.blueTable td, table.blueTable th {
   padding: 3px 2px;
 }
 table.blueTable tbody td {
-  font-size: 12px;
+  font-size: 14px;
 }
 table.blueTable tr:nth-child(even) {
   background: #D0E4F5;
@@ -48,7 +48,7 @@ table.blueTable tfoot {
   border-top: 2px solid #444444;
 }
 table.blueTable tfoot td {
-  font-size: 10px;
+  font-size: 12px;
 }
 table.blueTable tfoot .links {
   text-align: right;
@@ -72,14 +72,15 @@ $daten=rfcron($filename);
 
 
 <h1>Coffee Control</h1>
+<form id="Coffee-schedule" method="post">
 <table class="blueTable">
 <thead>
 <tr>
 <th>Tag</th>
-<th>Start Stunde</th>
-<th>Start Minute</th>
-<th>Stop Stunde</th>
-<th>Stop Minute</th>
+<th>Start h</th>
+<th>Start min</th>
+<th>Stop h</th>
+<th>Stop min</th>
 </tr>
 </thead>
 <tfoot>
@@ -92,210 +93,159 @@ $daten=rfcron($filename);
 <tr>
 <td>Montag</td>
 <td>
-<form id=Start-h-Mo>
 	<input name="start-h-MO" type="number" value=<?php echo $daten[0]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Start-m-Mo>
 	<input name="start-m-MO" type="number" value=<?php echo $daten[1]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-h-Mo>
 	<input name="stop-h-MO" type="number" value=<?php echo $daten[2]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-m-Mo>
 	<input name="stop-m-MO" type="number" value=<?php echo $daten[3]; ?> /> <br /> 
-</form>
 </td>
 </tr>
 <tr>
 <td>Dienstag</td>
 <td>
-<form id=Start-h-Di>
 	<input name="start-h-DI" type="number" value=<?php echo $daten[4]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Start-m-Di>
 	<input name="start-m-DI" type="number" value=<?php echo $daten[5]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-h-Di>
 	<input name="stop-h-DI" type="number" value=<?php echo $daten[6]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-m-Di>
 	<input name="stop-m-DI" type="number" value=<?php echo $daten[7]; ?> /> <br /> 
-</form>
 </td>
 </tr>
 <tr>
 <td>Mittwoch</td>
 <td>
-<form id=Start-h-Mi>
 	<input name="start-h-MI" type="number" value=<?php echo $daten[8]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Start-m-Mi>
 	<input name="start-m-MI" type="number" value=<?php echo $daten[9]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-h-Mi>
 	<input name="stop-h-MI" type="number" value=<?php echo $daten[10]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-m-Mi>
 	<input name="stop-m-MI" type="number" value=<?php echo $daten[11]; ?> /> <br /> 
-</form>
 </td>
 </tr>
 <tr>
 <td>Donnerstag</td>
 <td>
-<form id=Start-h-Do>
 	<input name="start-h-DO" type="number" value=<?php echo $daten[12]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Start-m-Do>
 	<input name="start-m-DO" type="number" value=<?php echo $daten[13]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-h-Do>
 	<input name="stop-h-DO" type="number" value=<?php echo $daten[14]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-m-Do>
 	<input name="stop-m-DO" type="number" value=<?php echo $daten[15]; ?> /> <br /> 
-</form>
 </td>
 </tr>
 <tr>
 <td>Freitag</td>
 <td>
-<form id=Start-h-Fr>
 	<input name="start-h-FR" type="number" value=<?php echo $daten[16]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Start-m-Fr>
 	<input name="start-m-FR" type="number" value=<?php echo $daten[17]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-h-Fr>
 	<input name="stop-h-FR" type="number" value=<?php echo $daten[18]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-m-Fr>
 	<input name="stop-m-FR" type="number" value=<?php echo $daten[19]; ?> /> <br /> 
-</form>
 </td>
 </tr>
 <tr>
 <td>Samstag</td>
 <td>
-<form id=Start-h-Sa>
 	<input name="start-h-SA" type="number" value=<?php echo $daten[20]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Start-m-Sa>
 	<input name="start-m-SA" type="number" value=<?php echo $daten[21]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-h-Sa>
 	<input name="stop-h-SA" type="number" value=<?php echo $daten[22]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-m-Sa>
 	<input name="stop-m-SA" type="number" value=<?php echo $daten[23]; ?> /> <br /> 
-</form>
 </td>
 </tr>
 <tr>
 <td>Sonntag</td>
 <td>
-<form id=Start-h-So>
 	<input name="start-h-SO" type="number" value=<?php echo $daten[24]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Start-m-So>
 	<input name="start-m-SO" type="number" value=<?php echo $daten[25]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-h-So>
 	<input name="stop-h-SO" type="number" value=<?php echo $daten[26]; ?> /> <br /> 
-</form>
 </td>
 <td>
-<form id=Stop-m-So>
 	<input name="stop-m-SO" type="number" value=<?php echo $daten[27]; ?> /> <br /> 
-</form>
 </td>
 </tr>
 </tbody>
 </table>
-<button form="Start-h-Mo" form="Start-m-Mo" form="Stop-h-Mo" form="Stop-m-Mo" form="Start-h-Di" form="Start-m-Di" form="Stop-h-Di" form="Stop-m-Di" form="Start-h-Mi" form="Start-m-Mi" form="Stop-h-Mi" form="Stop-m-Mi" form="Start-h-Do" form="Start-m-Do" form="Stop-h-Do" form="Stop-m-Do" form="Start-h-Fr" form="Start-m-Fr" form="Stop-h-Fr" form="Stop-m-Fr" form="Start-h-Sa" form="Start-m-Sa" form="Stop-h-Sa" form="Stop-m-Sa" form="Start-h-So" form="Start-m-So" form="Stop-h-So" form="Stop-m-So" type="submit" name='submitpressed' value='1'>Submit</button>
+<button form="Coffee-schedule" type="submit" method="post" name='submitpressed' value='1'>Submit</button>
 <button type="reset" value="Reset">Reset</button>
 
 <?php
 // POST-Formulardaten verarbeiten
+// ...
+//todo:
+// reset button implementieren
+// wieso weren nach dem Absenden des gänderten Formulars wieder die alten Werte angezeigt?!
+// ...
+
 $startstop = array();
-if (isset($_GET['submitpressed'])){
-   $new = $_GET['submitpressed'];
+if (isset($_POST['submitpressed'])){
+   $new = $_POST['submitpressed'];
    if ($new=='1'){
-     echo "Kaffee ist eingeschaltet!"; 
-     $startstop[0] = (int)$_GET['start-h-MO']; //OK
-     $startstop[1] = (int)$_GET['start-m-MO']; //ab hier undefined index?!
-     $startstop[2] = (int)$_GET['stop-h-MO'];
-     $startstop[3] = (int)$_GET['stop-m-MO'];
+     $startstop[0] = (int)$_POST['start-h-MO'];
+     $startstop[1] = (int)$_POST['start-m-MO'];
+     $startstop[2] = (int)$_POST['stop-h-MO'];
+     $startstop[3] = (int)$_POST['stop-m-MO'];
           
-     $startstop[4] = (int)$_GET['start-h-DI'];
-     $startstop[5] = (int)$_GET['start-m-DI'];
-     $startstop[6] = (int)$_GET['stop-h-DI'];
-     $startstop[7] = (int)$_GET['stop-m-DI'];
+     $startstop[4] = (int)$_POST['start-h-DI'];
+     $startstop[5] = (int)$_POST['start-m-DI'];
+     $startstop[6] = (int)$_POST['stop-h-DI'];
+     $startstop[7] = (int)$_POST['stop-m-DI'];
      
-     $startstop[8] = (int)$_GET['start-h-MI'];
-     $startstop[9] = (int)$_GET['start-m-MI'];
-     $startstop[10] = (int)$_GET['stop-h-MI'];
-     $startstop[11] = (int)$_GET['stop-m-MI'];
+     $startstop[8] = (int)$_POST['start-h-MI'];
+     $startstop[9] = (int)$_POST['start-m-MI'];
+     $startstop[10] = (int)$_POST['stop-h-MI'];
+     $startstop[11] = (int)$_POST['stop-m-MI'];
      
-     $startstop[12] = (int)$_GET["start-h-DO"];
-     $startstop[13] = (int)$_GET["start-m-DO"];
-     $startstop[14] = (int)$_GET["stop-h-DO"];
-     $startstop[15] = (int)$_GET["stop-m-DO"];
+     $startstop[12] = (int)$_POST['start-h-DO'];
+     $startstop[13] = (int)$_POST['start-m-DO'];
+     $startstop[14] = (int)$_POST['stop-h-DO'];
+     $startstop[15] = (int)$_POST['stop-m-DO'];
      
-     $startstop[16] = (int)$_GET["start-h-FR"];
-     $startstop[17] = (int)$_GET["start-m-FR"];
-     $startstop[18] = (int)$_GET["stop-h-FR"];
-     $startstop[19] = (int)$_GET["stop-m-FR"];
+     $startstop[16] = (int)$_POST['start-h-FR'];
+     $startstop[17] = (int)$_POST['start-m-FR'];
+     $startstop[18] = (int)$_POST['stop-h-FR'];
+     $startstop[19] = (int)$_POST['stop-m-FR'];
      
-     $startstop[20] = (int)$_GET["start-h-SA"];
-     $startstop[21] = (int)$_GET["start-m-SA"];
-     $startstop[22] = (int)$_GET["stop-h-SA"];
-     $startstop[23] = (int)$_GET["stop-m-SA"];
+     $startstop[20] = (int)$_POST['start-h-SA'];
+     $startstop[21] = (int)$_POST['start-m-SA'];
+     $startstop[22] = (int)$_POST['stop-h-SA'];
+     $startstop[23] = (int)$_POST['stop-m-SA'];
      
-     $startstop[24] = (int)$_GET["start-h-SO"];
-     $startstop[25] = (int)$_GET["start-m-SO"];
-     $startstop[26] = (int)$_GET["stop-h-SO"];
-     $startstop[27] = (int)$_GET["stop-m-SO"];
+     $startstop[24] = (int)$_POST['start-h-SO'];
+     $startstop[25] = (int)$_POST['start-m-SO'];
+     $startstop[26] = (int)$_POST['stop-h-SO'];
+     $startstop[27] = (int)$_POST['stop-m-SO'];
      
      for ($i=0; $i <= 27; $i++) {
      	echo "$startstop[$i] ";
--- filerw.php
+++ filerw.php
@@ -10,9 +10,9 @@ function &rfcron ($filename) {
     
     $startstop = array();
     $i = 0;
-    // $filename = './coffee-schedule';
-    $file = fopen( $filename, "r" );
-        
+    
+    $file = fopen( $filename, "r" );  
+    
     if( $file == false ) {
        echo ( "Error in opening file" );
        exit();
@@ -21,8 +21,7 @@ function &rfcron ($filename) {
 	while(!feof($file)) {
 		$startstop[$i++] = fgets($file);
 	}
-	
-    fclose( $file ); 
+	fclose( $file ); 
 	return $startstop;   
 }
 
@@ -51,6 +50,7 @@ function wfcron ($startstop, $filename) {
     
     while($i < count($startstop)) {
         fputs($file, $startstop[$i++]);
+        fputs($file, "\n");
     }
         
     fclose( $file );
@@ -64,37 +64,42 @@ function &reset_wfcron ($filename) {
 
 function createcron ($startstop) {
     //copy crontab into crontab-old and create a new crontab with new coffee-schedule
+    //when deploying on the target system enter the correct path here: /etc/crontab
     
     $rc = copy ('./crontab', './crontab.old');
+    if( $rc == false ) {
+    	echo ( "Error while copying file" );
+    	exit();
+    }
     
     $format = array();
-    $format[1] = "# /etc/crontab: system-wide crontab\n";
-    $format[2] = "# Unlike any other crontab you don't have to run the `crontab'\n";
-    $format[3] = "# command to install the new version when you edit this file\n";
-    $format[4] = "# and files in /etc/cron.d. These files also have username fields,\n";
-    $format[5] = "# that none of the other crontabs do.\n\n";
-    $format[6] = "SHELL=/bin/sh\n";
-    $format[7] = "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n";
-    $format[8] = "# m h dom mon dow user	command\n\n";
-    $format[9] = "17 *	* * *	root    cd / && run-parts --report /etc/cron.hourly\n";
-    $format[10] = "25 6	* * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.daily )\n";
-    $format[11] = "47 6	* * 7	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )\n";
-    $format[12] = "52 6	1 * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )\n";
-    $format[13] = "$startstop[1] $startstop[2] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
-    $format[14] = "$startstop[3] $startstop[4] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
-    $format[15] = "$startstop[5] $startstop[6] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
-    $format[16] = "$startstop[7] $startstop[8] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
-    $format[17] = "$startstop[9] $startstop[10] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
-    $format[18] = "$startstop[11] $startstop[12] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
-    $format[19] = "$startstop[13] $startstop[14] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
-    $format[20] = "$startstop[15] $startstop[16] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
-    $format[21] = "$startstop[17] $startstop[18] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
-    $format[22] = "$startstop[19] $startstop[20] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
-    $format[23] = "$startstop[21] $startstop[22] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
-    $format[24] = "$startstop[23] $startstop[24] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
-    $format[25] = "$startstop[25] $startstop[26] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
-    $format[26] = "$startstop[27] $startstop[28] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
-    $format[27] = "#";  
+    $format[0] = "# /etc/crontab: system-wide crontab\n";
+    $format[1] = "# Unlike any other crontab you don't have to run the `crontab'\n";
+    $format[2] = "# command to install the new version when you edit this file\n";
+    $format[3] = "# and files in /etc/cron.d. These files also have username fields,\n";
+    $format[4] = "# that none of the other crontabs do.\n\n";
+    $format[5] = "SHELL=/bin/sh\n";
+    $format[6] = "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n";
+    $format[7] = "# m h dom mon dow user	command\n\n";
+    $format[8] = "17 *	* * *	root    cd / && run-parts --report /etc/cron.hourly\n";
+    $format[9] = "25 6	* * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.daily )\n";
+    $format[10] = "47 6	* * 7	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )\n";
+    $format[11] = "52 6	1 * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )\n";
+    $format[12] = "$startstop[0] $startstop[1] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[13] = "$startstop[2] $startstop[3] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[14] = "$startstop[4] $startstop[5] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[15] = "$startstop[6] $startstop[7] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[16] = "$startstop[8] $startstop[9] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[17] = "$startstop[10] $startstop[11] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[18] = "$startstop[12] $startstop[13] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[19] = "$startstop[14] $startstop[15] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[20] = "$startstop[16] $startstop[17] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[21] = "$startstop[18] $startstop[19] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[22] = "$startstop[20] $startstop[21] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[23] = "$startstop[22] $startstop[23] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[24] = "$startstop[24] $startstop[25] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[25] = "$startstop[26] $startstop[27] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[26] = "#";  
       
     echo file_put_contents("crontab", $format);       
 }
