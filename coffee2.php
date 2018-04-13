6031836ef9dc41ddb2b90d85015360fd31546c16
mike69er <Waake1940@einrot.com>
2018-04-13 23:09:32 +0200

    filerw.php stable, coffee2.php not working

--- coffee2.php
+++ coffee2.php
@@ -65,21 +65,9 @@ table.blueTable tfoot .links a{
 
 
 <?php
-/*
-// POST-Formulardaten verarbeiten
-if (isset($_POST['submitpressed'])){
-   $new = $_POST['submitpressed'];
-   if ($new=='1'){
-     echo "Kaffee ist eingeschaltet!"; 
-   }
-}
-
-*/
-?>
-
-<?php
 require ('filerw.php');
 $filename = "./coffee-schedule";
+$daten=rfcron($filename);
 ?>
 
 
@@ -105,22 +93,22 @@ $filename = "./coffee-schedule";
 <td>Montag</td>
 <td>
 <form id=Start-h-Mo>
-	<input name="start-h-MO" type="number" value=<?php $daten=rfcron($filename); echo $daten[0]; ?> /> <br /> 
+	<input name="start-h-MO" type="number" value=<?php echo $daten[0]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Start-m-Mo>
-	<input name="start-m-MO" type="number" value=<?php $daten=rfcron($filename); echo $daten[1]; ?> /> <br /> 
+	<input name="start-m-MO" type="number" value=<?php echo $daten[1]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-h-Mo>
-	<input name="stop-h-MO" type="number" value=<?php $daten=rfcron($filename); echo $daten[2]; ?> /> <br /> 
+	<input name="stop-h-MO" type="number" value=<?php echo $daten[2]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-m-Mo>
-	<input name="stop-m-MO" type="number" value=<?php $daten=rfcron($filename); echo $daten[3]; ?> /> <br /> 
+	<input name="stop-m-MO" type="number" value=<?php echo $daten[3]; ?> /> <br /> 
 </form>
 </td>
 </tr>
@@ -128,22 +116,22 @@ $filename = "./coffee-schedule";
 <td>Dienstag</td>
 <td>
 <form id=Start-h-Di>
-	<input name="start-h-DI" type="number" value=<?php $daten=rfcron($filename); echo $daten[4]; ?> /> <br /> 
+	<input name="start-h-DI" type="number" value=<?php echo $daten[4]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Start-m-Di>
-	<input name="start-m-DI" type="number" value=<?php $daten=rfcron($filename); echo $daten[5]; ?> /> <br /> 
+	<input name="start-m-DI" type="number" value=<?php echo $daten[5]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-h-Di>
-	<input name="stop-h-DI" type="number" value=<?php $daten=rfcron($filename); echo $daten[6]; ?> /> <br /> 
+	<input name="stop-h-DI" type="number" value=<?php echo $daten[6]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-m-Di>
-	<input name="stop-m-DI" type="number" value=<?php $daten=rfcron($filename); echo $daten[7]; ?> /> <br /> 
+	<input name="stop-m-DI" type="number" value=<?php echo $daten[7]; ?> /> <br /> 
 </form>
 </td>
 </tr>
@@ -151,22 +139,22 @@ $filename = "./coffee-schedule";
 <td>Mittwoch</td>
 <td>
 <form id=Start-h-Mi>
-	<input name="start-h-MI" type="number" value=<?php $daten=rfcron($filename); echo $daten[8]; ?> /> <br /> 
+	<input name="start-h-MI" type="number" value=<?php echo $daten[8]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Start-m-Mi>
-	<input name="start-m-MI" type="number" value=<?php $daten=rfcron($filename); echo $daten[9]; ?> /> <br /> 
+	<input name="start-m-MI" type="number" value=<?php echo $daten[9]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-h-Mi>
-	<input name="stop-h-MI" type="number" value=<?php $daten=rfcron($filename); echo $daten[10]; ?> /> <br /> 
+	<input name="stop-h-MI" type="number" value=<?php echo $daten[10]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-m-Mi>
-	<input name="stop-m-MI" type="number" value=<?php $daten=rfcron($filename); echo $daten[11]; ?> /> <br /> 
+	<input name="stop-m-MI" type="number" value=<?php echo $daten[11]; ?> /> <br /> 
 </form>
 </td>
 </tr>
@@ -174,22 +162,22 @@ $filename = "./coffee-schedule";
 <td>Donnerstag</td>
 <td>
 <form id=Start-h-Do>
-	<input name="start-h-DO" type="number" value=<?php $daten=rfcron($filename); echo $daten[12]; ?> /> <br /> 
+	<input name="start-h-DO" type="number" value=<?php echo $daten[12]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Start-m-Do>
-	<input name="start-m-DO" type="number" value=<?php $daten=rfcron($filename); echo $daten[13]; ?> /> <br /> 
+	<input name="start-m-DO" type="number" value=<?php echo $daten[13]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-h-Do>
-	<input name="stop-h-DO" type="number" value=<?php $daten=rfcron($filename); echo $daten[14]; ?> /> <br /> 
+	<input name="stop-h-DO" type="number" value=<?php echo $daten[14]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-m-Do>
-	<input name="stop-m-DO" type="number" value=<?php $daten=rfcron($filename); echo $daten[15]; ?> /> <br /> 
+	<input name="stop-m-DO" type="number" value=<?php echo $daten[15]; ?> /> <br /> 
 </form>
 </td>
 </tr>
@@ -197,22 +185,22 @@ $filename = "./coffee-schedule";
 <td>Freitag</td>
 <td>
 <form id=Start-h-Fr>
-	<input name="start-h-FR" type="number" value=<?php $daten=rfcron($filename); echo $daten[16]; ?> /> <br /> 
+	<input name="start-h-FR" type="number" value=<?php echo $daten[16]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Start-m-Fr>
-	<input name="start-m-FR" type="number" value=<?php $daten=rfcron($filename); echo $daten[17]; ?> /> <br /> 
+	<input name="start-m-FR" type="number" value=<?php echo $daten[17]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-h-Fr>
-	<input name="stop-h-FR" type="number" value=<?php $daten=rfcron($filename); echo $daten[18]; ?> /> <br /> 
+	<input name="stop-h-FR" type="number" value=<?php echo $daten[18]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-m-Fr>
-	<input name="stop-m-FR" type="number" value=<?php $daten=rfcron($filename); echo $daten[19]; ?> /> <br /> 
+	<input name="stop-m-FR" type="number" value=<?php echo $daten[19]; ?> /> <br /> 
 </form>
 </td>
 </tr>
@@ -220,22 +208,22 @@ $filename = "./coffee-schedule";
 <td>Samstag</td>
 <td>
 <form id=Start-h-Sa>
-	<input name="start-h-SA" type="number" value=<?php $daten=rfcron($filename); echo $daten[20]; ?> /> <br /> 
+	<input name="start-h-SA" type="number" value=<?php echo $daten[20]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Start-m-Sa>
-	<input name="start-m-SA" type="number" value=<?php $daten=rfcron($filename); echo $daten[21]; ?> /> <br /> 
+	<input name="start-m-SA" type="number" value=<?php echo $daten[21]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-h-Sa>
-	<input name="stop-h-SA" type="number" value=<?php $daten=rfcron($filename); echo $daten[22]; ?> /> <br /> 
+	<input name="stop-h-SA" type="number" value=<?php echo $daten[22]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-m-Sa>
-	<input name="stop-m-SA" type="number" value=<?php $daten=rfcron($filename); echo $daten[23]; ?> /> <br /> 
+	<input name="stop-m-SA" type="number" value=<?php echo $daten[23]; ?> /> <br /> 
 </form>
 </td>
 </tr>
@@ -243,42 +231,80 @@ $filename = "./coffee-schedule";
 <td>Sonntag</td>
 <td>
 <form id=Start-h-So>
-	<input name="start-h-SO" type="number" value=<?php $daten=rfcron($filename); echo $daten[24]; ?> /> <br /> 
+	<input name="start-h-SO" type="number" value=<?php echo $daten[24]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Start-m-So>
-	<input name="start-m-SO" type="number" value=<?php $daten=rfcron($filename); echo $daten[25]; ?> /> <br /> 
+	<input name="start-m-SO" type="number" value=<?php echo $daten[25]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-h-So>
-	<input name="stop-h-SO" type="number" value=<?php $daten=rfcron($filename); echo $daten[26]; ?> /> <br /> 
+	<input name="stop-h-SO" type="number" value=<?php echo $daten[26]; ?> /> <br /> 
 </form>
 </td>
 <td>
 <form id=Stop-m-So>
-	<input name="stop-m-SO" type="number" value=<?php $daten=rfcron($filename); echo $daten[27]; ?> /> <br /> 
+	<input name="stop-m-SO" type="number" value=<?php echo $daten[27]; ?> /> <br /> 
 </form>
 </td>
 </tr>
 </tbody>
 </table>
-<button form="Start-h-Mo" form="Start-m-Mo" type="submit" name='submitpressed' value='1'>Submit</button>
+<button form="Start-h-Mo" form="Start-m-Mo" form="Stop-h-Mo" form="Stop-m-Mo" form="Start-h-Di" form="Start-m-Di" form="Stop-h-Di" form="Stop-m-Di" form="Start-h-Mi" form="Start-m-Mi" form="Stop-h-Mi" form="Stop-m-Mi" form="Start-h-Do" form="Start-m-Do" form="Stop-h-Do" form="Stop-m-Do" form="Start-h-Fr" form="Start-m-Fr" form="Stop-h-Fr" form="Stop-m-Fr" form="Start-h-Sa" form="Start-m-Sa" form="Stop-h-Sa" form="Stop-m-Sa" form="Start-h-So" form="Start-m-So" form="Stop-h-So" form="Stop-m-So" type="submit" name='submitpressed' value='1'>Submit</button>
 <button type="reset" value="Reset">Reset</button>
 
 <?php
-
 // POST-Formulardaten verarbeiten
-if (isset($_POST['submitpressed'])){
-   $new = $_POST['submitpressed'];
+$startstop = array();
+if (isset($_GET['submitpressed'])){
+   $new = $_GET['submitpressed'];
    if ($new=='1'){
      echo "Kaffee ist eingeschaltet!"; 
+     $startstop[0] = (int)$_GET['start-h-MO']; //OK
+     $startstop[1] = (int)$_GET['start-m-MO']; //ab hier undefined index?!
+     $startstop[2] = (int)$_GET['stop-h-MO'];
+     $startstop[3] = (int)$_GET['stop-m-MO'];
+          
+     $startstop[4] = (int)$_GET['start-h-DI'];
+     $startstop[5] = (int)$_GET['start-m-DI'];
+     $startstop[6] = (int)$_GET['stop-h-DI'];
+     $startstop[7] = (int)$_GET['stop-m-DI'];
+     
+     $startstop[8] = (int)$_GET['start-h-MI'];
+     $startstop[9] = (int)$_GET['start-m-MI'];
+     $startstop[10] = (int)$_GET['stop-h-MI'];
+     $startstop[11] = (int)$_GET['stop-m-MI'];
+     
+     $startstop[12] = (int)$_GET["start-h-DO"];
+     $startstop[13] = (int)$_GET["start-m-DO"];
+     $startstop[14] = (int)$_GET["stop-h-DO"];
+     $startstop[15] = (int)$_GET["stop-m-DO"];
+     
+     $startstop[16] = (int)$_GET["start-h-FR"];
+     $startstop[17] = (int)$_GET["start-m-FR"];
+     $startstop[18] = (int)$_GET["stop-h-FR"];
+     $startstop[19] = (int)$_GET["stop-m-FR"];
+     
+     $startstop[20] = (int)$_GET["start-h-SA"];
+     $startstop[21] = (int)$_GET["start-m-SA"];
+     $startstop[22] = (int)$_GET["stop-h-SA"];
+     $startstop[23] = (int)$_GET["stop-m-SA"];
+     
+     $startstop[24] = (int)$_GET["start-h-SO"];
+     $startstop[25] = (int)$_GET["start-m-SO"];
+     $startstop[26] = (int)$_GET["stop-h-SO"];
+     $startstop[27] = (int)$_GET["stop-m-SO"];
+     
+     for ($i=0; $i <= 27; $i++) {
+     	echo "$startstop[$i] ";
+     }
+     
+     wfcron($startstop, $filename);
+     createcron($startstop);
    }
 }
-
 ?>
-
-<h2>Coffee Control</h2>
 
 
--- filerw.php
+++ filerw.php
@@ -1,8 +1,13 @@
 <?php
 // functions to read/write file and change crontab
+// author: M.Pühlhöfer, 2018
 
 
 function &rfcron ($filename) {
+    // read file containing start-stop values
+    // format: Mo_start_h, Mo_start_min, Mo_stop_h, Mo_stop_min, Di_start_h. Di_start_min, ... 
+    // return array of start-stop values
+    
     $startstop = array();
     $i = 0;
     // $filename = './coffee-schedule';
@@ -23,6 +28,10 @@ function &rfcron ($filename) {
 
 
 function wfcron ($startstop, $filename) {
+	// write file containing start-stop values
+	// format: Mo_start_h, Mo_start_min, Mo_stop_h, Mo_stop_min, Di_start_h. Di_start_min, ...
+	// the last start-stop values before changing are stored in "coffee-before" (to enable reset)
+	
     $i = 0;
     //$filename = './coffee-schedule';
     
@@ -43,8 +52,51 @@ function wfcron ($startstop, $filename) {
     while($i < count($startstop)) {
         fputs($file, $startstop[$i++]);
     }
-    
+        
     fclose( $file );
+}
+
+function &reset_wfcron ($filename) {
+    // reset to start-stop values before recent change
+    
+    return rfcron ($filename);
+}
+
+function createcron ($startstop) {
+    //copy crontab into crontab-old and create a new crontab with new coffee-schedule
+    
+    $rc = copy ('./crontab', './crontab.old');
+    
+    $format = array();
+    $format[1] = "# /etc/crontab: system-wide crontab\n";
+    $format[2] = "# Unlike any other crontab you don't have to run the `crontab'\n";
+    $format[3] = "# command to install the new version when you edit this file\n";
+    $format[4] = "# and files in /etc/cron.d. These files also have username fields,\n";
+    $format[5] = "# that none of the other crontabs do.\n\n";
+    $format[6] = "SHELL=/bin/sh\n";
+    $format[7] = "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n";
+    $format[8] = "# m h dom mon dow user	command\n\n";
+    $format[9] = "17 *	* * *	root    cd / && run-parts --report /etc/cron.hourly\n";
+    $format[10] = "25 6	* * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.daily )\n";
+    $format[11] = "47 6	* * 7	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )\n";
+    $format[12] = "52 6	1 * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )\n";
+    $format[13] = "$startstop[1] $startstop[2] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[14] = "$startstop[3] $startstop[4] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[15] = "$startstop[5] $startstop[6] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[16] = "$startstop[7] $startstop[8] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[17] = "$startstop[9] $startstop[10] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[18] = "$startstop[11] $startstop[12] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[19] = "$startstop[13] $startstop[14] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[20] = "$startstop[15] $startstop[16] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[21] = "$startstop[17] $startstop[18] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[22] = "$startstop[19] $startstop[20] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[23] = "$startstop[21] $startstop[22] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[24] = "$startstop[23] $startstop[24] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[25] = "$startstop[25] $startstop[26] * * 1	root	/home/pi/Projects/remote-control/start-coffee\n";
+    $format[26] = "$startstop[27] $startstop[28] * * 1	root	/home/pi/Projects/remote-control/stop-coffee\n";
+    $format[27] = "#";  
+      
+    echo file_put_contents("crontab", $format);       
 }
 
 
